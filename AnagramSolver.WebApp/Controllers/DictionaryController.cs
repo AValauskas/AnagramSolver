ef42d3723830834a2caf407d0226a878b2c82745
Alauskas <37072295+Alauskas@users.noreply.github.com>
2020-07-28 11:00:33 +0300

    Pagination made

---
+++ AnagramSolver.WebApp/Controllers/DictionaryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnagramSolver.Contracts.Interfaces;
+using AnagramSolver.Contracts.Models;
+using AnagramSolver.WebApp.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnagramSolver.WebApp.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        private readonly IWordRepository _wordRepository;
+        public DictionaryController(IWordRepository wordRepository)
+        {
+            _wordRepository = wordRepository;
+        }
+
+        public IActionResult Index(int? pageNumber)
+        {
+            var words = _wordRepository.GetAllWords();
+            int pageSize = 100;
+            return View(PaginatedList<Anagram>.CreateAsync(words, pageNumber ?? 1, pageSize));
+
+        }
+    }
+}
--- AnagramSolver.WebApp/Controllers/HomeController.cs
+++ AnagramSolver.WebApp/Controllers/HomeController.cs
@@ -1,32 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AnagramSolver.WebApp.Models;
-using AnagramSolver.BusinessLogic;
 using AnagramSolver.Contracts.Interfaces;
 
 namespace AnagramSolver.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IAnagramSolver _anagramSolver;
-        private readonly IWordRepository _wordRepository;
-        public HomeController(ILogger<HomeController> logger, IAnagramSolver anagramSolver, IWordRepository wordRepository)
+        public HomeController(IAnagramSolver anagramSolver)
         {
             _anagramSolver = anagramSolver;
-            _logger = logger;
-            _wordRepository = wordRepository;
         }
 
     
-        public IActionResult Index(string id)
+        public IActionResult Index(string word)
         {
-            var anagrams = _anagramSolver.GetAnagrams(id);
+            var anagrams = _anagramSolver.GetAnagrams(word);
             if (anagrams==null)
             {
                 anagrams = new List<string>();
@@ -44,12 +36,5 @@ namespace AnagramSolver.WebApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Dictionary()
-        {
-            var words = _wordRepository.GetAllWords();
-
-
-            return View(words);
-        }
     }
 }
