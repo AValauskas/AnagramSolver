6aa9c937238e011bc976917e35ddce433835f653
Alauskas <37072295+Alauskas@users.noreply.github.com>
2020-08-17 15:50:20 +0300

    Implemented Soap calculator

---
+++ SoapConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using ServiceReference1;
+using System;
+using System.Threading.Tasks;
+
+namespace SoapConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {           
+            var conf = new CalculatorSoapClient.EndpointConfiguration();
+            var calculator = new CalculatorSoapClient(conf);
+            await SoapLogic(calculator);
+        }
+
+        public static async Task SoapLogic(CalculatorSoapClient calculator)
+        {
+            while (true)
+            {              
+                Console.WriteLine("Choose action (+ - / *) x - to exit");
+                var act = Console.ReadLine();
+
+                if (act == "x")
+                    break;
+                int num1, num2;                
+                Console.WriteLine("Write first number");
+                if (!int.TryParse(Console.ReadLine(), out num1))
+                {
+                    Console.WriteLine("You must write number in numbers part\n");
+                    continue;
+                }
+               
+                Console.WriteLine("Write Second number");
+                if (!int.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("You must write number in numbers part");
+                    continue;
+                }
+
+                switch (act)
+                {
+                    case "+":
+                        Console.WriteLine($"{num1} + {num2} = {await calculator.AddAsync(num1, num2)}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{num1} - {num2} = {await calculator.SubtractAsync(num1, num2)}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{num1} * {num2} = {await calculator.MultiplyAsync(num1, num2)}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{num1} / {num2} = {await calculator.DivideAsync(num1, num2)}");
+                        break;                
+                      
+                    default:
+                        Console.WriteLine("wrong action was written ");
+                        break;
+                }
+                Console.WriteLine("Press any to continue");
+                Console.ReadLine();
+            }
+        }
+    }
+}
